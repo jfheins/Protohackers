aedd99a206b3c547dcf8fe1682ad92029dca91a5
Julius Heins <jfheins@gmail.com>
2022-09-19 22:22:35 +0200

    Refactor

---
+++ Core/ArbitraryChunkHandler.cs
@@ -0,0 +1,28 @@
+﻿namespace Core;
+
+public abstract class ArbitraryChunkHandler : Handler
+{
+    protected abstract Task HandleChunk(ReadOnlyMemory<byte> chunk);
+
+    public override async Task HandleClient()
+    {
+        while (true)
+        {
+            var result = await Reader.ReadAsync();
+            var buffer = result.Buffer;
+
+            foreach (var chunk in buffer)
+                await HandleChunk(chunk);
+
+            // Tell the PipeReader how much of the buffer has been consumed.
+            Reader.AdvanceTo(buffer.Start, buffer.End);
+
+            // Stop reading if there's no more data coming.
+            if (result.IsCompleted)
+            {
+                Console.WriteLine("completed");
+                break;
+            }
+        }
+    }
+}
